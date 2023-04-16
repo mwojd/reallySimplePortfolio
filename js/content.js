28e24e87f18870489f201f2c09aebb870bc0b186
mwojd <wojdaszewiczmikolaj@gmail.com>
2023-04-17 01:01:29 +0200

    first and final commit

---
+++ js/content.js
@@ -0,0 +1,25 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById("cont")
+    const featuredBut = document.getElementById("cat1")
+    const projectsButt = document.getElementById("cat2")
+    const faqButt = document.getElementById("cat3")
+    const contactButt = document.getElementById("cat4")
+    featuredBut.addEventListener("click", () => {
+        content.innerHTML = '<h2><a href="https://github.com/mwojd/manipulatedFortuneWheel" target="_blank">a fortune wheel with manipulated odds -html/css/js</br></a></h2>'
+    })
+    projectsButt.addEventListener("click", () => {
+        content.innerHTML='<h3><a href="https://github.com/mwojd/manipulatedFortuneWheel" target="_blank">a fortune wheel with manipulated odds - c++</a></br>\
+        <a href="https://github.com/mwojd/ProjektSystemy" target="_blank">a simple "presentation" about operating systems -html/css/js</a></h3>'
+    })
+    faqButt.addEventListener("click", () => {
+        content.innerHTML='Q:do you take commisions?</br>A:since I am a student I do not have any time to spend however if you can give me a good price then I will surely do it!<br>\
+        <br>Q:would you say that you are experienced or advanced in your field?</br>A:no! not at all! I still have a lot to learn</br> '
+    })
+    contactButt.addEventListener("click", () => {
+        content.innerHTML= '<h1>discord: teev#9322</br>\
+        mail: wojdaszewiczmikolaj@gmail.com</br>\
+        <a href="https://github.com/mwojd" target="_blank">github: @mwojd</a></h1>';
+    })
+});
