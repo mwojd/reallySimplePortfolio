03145e9ac8bb1291fa4032c7eca17a59a6bc0fe8
mwojd <wojdaszewiczmikolaj@gmail.com>
2023-04-17 07:01:42 +0200

    accidentaly deleted two brackets, added back

--- js/content.js
+++ js/content.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <h2><a href="https://github.com/mwojd/manipulatedFortuneWheel" target="_blank">a fortune wheel with manipulated odds -html/css/js</br></a>\
         <a href="https://github.com/mwojd/hackinggame" target="_blank">a very simple game to learn the very basics of memory hacking - c++</a></br>\
         </h2>'
+    })
     projectsButt.addEventListener("click", () => {
         content.innerHTML='<h1><a href="https://github.com/mwojd/reallySimplePortfolio" target="_blank">THIS website - html/css/js</a></h1><h3></br>\
         <a href="https://github.com/mwojd/hackinggame" target="_blank">a very simple game to learn the very basics of memory hacking - c++</a></br>\
@@ -21,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     faqButt.addEventListener("click", () => {
         content.innerHTML='Q:do you take commisions?</br>A:since I am a student I do not have any time to spend however if you can give me a good price then I will surely do it!<br>\
         <br>Q:would you say that you are experienced or advanced in your field?</br>A:no! not at all! I still have a lot to learn</br> '
-    })git commit -m "added another project to the list"
+    })
     contactButt.addEventListener("click", () => {
         content.innerHTML= '<h1>discord: teev#9322</br>\
         mail: wojdaszewiczmikolaj@gmail.com</br>\
